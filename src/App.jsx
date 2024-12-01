4e0d2e05ceaabee4781f40ecf3269b7ae98d5a94
Nicholas Falkman Doxander <nicholas.falkman@hotmail.se>
2024-12-01 11:24:13 +0100

    Tailwindcss setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <h1 className='text-3xl font-bold underline'>testing!</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
