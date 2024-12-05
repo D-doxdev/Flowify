424339a75d7defd7845ce700ab218ef24b8ac851
Nicholas Falkman Doxander <nicholas.falkman@hotmail.se>
2024-12-06 00:33:15 +0100

    Add login button and basic Spotify API implicit grant flow auth

--- spotify_API_data.js
+++ spotify_API_data.js
@@ -1,20 +1,28 @@
-
-
-/*
 var redirect_uri = "http://localhost:5173/";
 
-var state = generateRandomString(16);
+//var state = generateRandomString(16);
 
-localStorage.setItem(stateKey, state);
-var scope =
-  "user-read-private user-read-email playlist-modify-private playlist-modify-public user-library-modify";
+//localStorage.setItem(stateKey, state);
+var scope = "user-read-private playlist-modify-private playlist-modify-public user-library-modify";
 
 var url = "https://accounts.spotify.com/authorize";
 url += "?response_type=token";
-url += "&client_id=" + encodeURIComponent(client_id);
+url += "&client_id=" + encodeURIComponent(import.meta.env.VITE_CLIENT_ID);
 url += "&scope=" + encodeURIComponent(scope);
 url += "&redirect_uri=" + encodeURIComponent(redirect_uri);
-url += "&state=" + encodeURIComponent(state);
-*/
+//url += "&state=" + encodeURIComponent(state);
 
+const getReturnedParamsFromSpotifyAuth = (hash) => {
+  const stringAfterHashtag = hash.substring(1);
+  const paramsInUrl = stringAfterHashtag.split("&");
+  const paramsSplitUp = paramsInUrl.reduce((accumulater, currentValue) => {
+    console.log(currentValue);
+    const [key, value] = currentValue.split("=");
+    accumulater[key] = value;
+    return accumulater;
+  }, {});
 
+  return paramsSplitUp;
+};
+
+export {url, getReturnedParamsFromSpotifyAuth}
--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ListsContainer from "./ListsContainer/ListsContainer";
 import React, { useEffect, useState } from "react";
 import { ShaderGradientCanvas, ShaderGradient } from "@shadergradient/react";
 import SearchBarComponent from "./SearchBarComponent";
+import { url, getReturnedParamsFromSpotifyAuth } from "../spotify_API_data"
 //import axios from "axios";
 
 function App() {
@@ -30,7 +31,28 @@ function App() {
     } catch (error) {
       console.error(error);
     }
+    console.log(url)
   }, []);
+
+  // State for checking if logged in, should also check against the expires token which will be added. Need to improve security, also fix a state random number generator for spotify API.
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    if (window.location.hash) {
+      const { access_token, expires_in, token_type } =
+        getReturnedParamsFromSpotifyAuth(window.location.hash);
+
+      localStorage.clear();
+
+      localStorage.setItem("accessToken", access_token);
+      localStorage.setItem("tokenType", token_type);
+      localStorage.setItem("expiresIn", expires_in);
+      if (localStorage.getItem("accessToken")) {
+        setIsLoggedIn(true)
+      } else {
+        setIsLoggedIn(false)
+      }
+    }
+  }, []); // added a dependancy array, different from the tutorial
 
   // search function for finding songs! I left off here 4 Dec 2024. https://www.youtube.com/watch?v=1PWDxgqLmDA
   async function search() {
@@ -64,11 +86,15 @@ function App() {
     } catch (error) {
       console.log('Song request failed! Please enter an artist name into the search field or reload the webpage.')
     }
-    
-    
   }
 
+  // Feeds the url from spotify_API_data to user browser when login button is clicked
+  const handleLogin = () => {
+    window.location = url;
+  };
+
   return (
+    isLoggedIn ? 
     <div className="h-dvh w-full overflow-hidden px-10">
       <ShaderGradientCanvas
         style={{
@@ -92,7 +118,11 @@ function App() {
       musicSearchResult={musicSearchResult}
       />
     </div>
+    :
+    <button className="text-white" onClick={handleLogin}>login to spotify</button>
+
   );
 }
 
 export default App;
+
